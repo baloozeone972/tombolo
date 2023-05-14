c94ccbc9de82e29c0ed0a4c78991512855ad9057
Baloo <dev.baloozeone@gmail.com>
2023-05-14 19:15:16 +0200

    spring boot init

---
+++ src/main/java/mq/bzodev/tombolo/TomboloApplication.java
@@ -0,0 +1,13 @@
+package mq.bzodev.tombolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomboloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomboloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mq/bzodev/tombolo/TomboloApplicationTests.java
@@ -0,0 +1,13 @@
+package mq.bzodev.tombolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomboloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
